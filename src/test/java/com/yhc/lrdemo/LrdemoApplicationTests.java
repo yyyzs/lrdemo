144a709c110f63b811043f92ebe5470e9bf56d5f
yhc <yanghc95@163.com>
2020-01-01 21:52:21 +0800

    test

---
+++ src/test/java/com/yhc/lrdemo/LrdemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yhc.lrdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LrdemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
